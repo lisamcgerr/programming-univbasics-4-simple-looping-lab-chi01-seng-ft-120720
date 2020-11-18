428a3b369c63093502b4bee973a5b6955083a69c
Lisa Marie McGerr <lisamarie.mcgerr@gmail.com>
2020-11-18 16:35:12 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -23,7 +23,16 @@ def output_array(array)
   end
 end
 
-def return_string_array(array)
-  
+array = ["zero", "one", "two", "three", "four", "five", "six"]
 
+def return_string_array(array)
+  count = 0 
+  new_array = []
+  while count < array.length do 
+    new_array.push(array[count]).to_s 
+    count += 1 
+  end
+  new_array
 end
+
+return_string_array(array)
