58ed40053b26afea5253e558827c2cb6a8e0acce
Lisa Marie McGerr <lisamarie.mcgerr@gmail.com>
2020-11-18 16:12:17 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,16 @@
-# Write your methods here
+def loop_message_five_times(string)
+  puts string
+  puts string
+  puts string
+  puts string
+  puts string
+end
+
+def loop_message_n_times(string, integer)
+
+while integer.times do
+  puts string
+end
+end
+
+loop_message_n_times("Hi Sam!", 5)
